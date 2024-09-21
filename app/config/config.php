0c93fd83df6b9f7f1ad76f30dc4019bbec1d41a7
Huszka Adrian <adrian.huszka@gmail.com>
2024-09-21 14:41:28 +0200

    Added autoupdate

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,8 @@
     define('APPROOT', dirname(dirname(__FILE__)));
 
     //Load .env file
-    (new DotEnv(dirname(APPROOT) . '/.env'))->load();
+    if(!getenv('PRODUCTION'))
+        (new DotEnv(dirname(APPROOT) . '/.env'))->load();
 
     //Database params
     define('DB_HOST', getenv('DB_HOST'));
