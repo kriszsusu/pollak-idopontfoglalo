446189db084555a0c2d63af51cc91c0e6ea0078f
Huszka Adrian <adrian.huszka@gmail.com>
2024-09-22 12:30:56 +0200

    Refactor .env.example and remove database configuration

--- app/config/config.php
+++ app/config/config.php
@@ -2,23 +2,21 @@
     //APPROOT
     define('APPROOT', dirname(dirname(__FILE__)));
 
-    //Load .env file
-    if(!getenv('PRODUCTION'))
-        (new DotEnv(dirname(APPROOT) . '/.env'))->load();
+    if(getenv('PRODUCTION')) {
+        define('DB_HOST', getenv('DB_HOST'));
+        define('DB_USER', getenv('DB_USER'));
+        define('DB_PASS', getenv('DB_PASS'));
+        define('DB_NAME', getenv('DB_NAME'));
 
-    //Database params
-    define('DB_HOST', getenv('DB_HOST'));
-    define('DB_USER', getenv('DB_USER'));
-    define('DB_PASS', getenv('DB_PASS'));
-    define('DB_NAME', getenv('DB_NAME'));
-
-    
-
-    //URLROOT (Dynamic links)
-    if(!getenv('PRODUCTION'))
-        define('URLROOT', 'http://localhost/pollak-idopontfoglalo');
-    else
         define('URLROOT', 'https://foglalas.pollak.info');
+    } else {
+        define('DB_HOST', 'localhost');
+        define('DB_USER', 'root');
+        define('DB_PASS', '');
+        define('DB_NAME', 'pollakidopontfoglalas');
+
+        define('URLROOT', 'http://localhost/pollak-idopontfoglalo');
+    }      
 
     //Sitename
     define('SITENAME', 'pollak-idopontfoglalo');
--- app/require.php
+++ app/require.php
@@ -8,7 +8,6 @@ require_once 'libraries/Controller.php';
 require_once 'libraries/Database.php';
 
 require_once 'helpers/session_helper.php';
-require_once 'helpers/dot_env_loader.php';
 
 require_once 'config/config.php';
 
