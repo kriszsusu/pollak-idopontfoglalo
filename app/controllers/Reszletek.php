ec6bb5756cc2f76b3430f3eda3ad61aa7441ed28
LakatosRonaldo <106381433+LakatosRonaldo@users.noreply.github.com>
2024-09-26 11:12:27 +0200

    Lehetséges email block fix

--- app/controllers/Reszletek.php
+++ app/controllers/Reszletek.php
@@ -22,16 +22,32 @@ class Reszletek extends Controller {
 
             $esemenyID = (int)trim($_POST['esemenyID']);
             $email = trim($_POST['email']);
-
-            if ($this->reszletekModel->emailHozzadas($esemenyID, $email)) {
-                // A hozzáadás sikerült, átirányítjuk a felhasználót a főoldalra
-                header('location:' . URLROOT . '/main');
-            }
-            else {
-                // A hozzáadás nem sikerült ezért vissza irányítjuk a főoldlra
-                header('location:' . URLROOT . '/main');
+            //Tiltott email-ek beolvasása            
+            $json = file_get_contents('emailBlock.json');
+            
+                if ($json === false){
+                    die('Nem sikerült beolvasni a JSON file-t');
+                }
+            $json_data = json_decode($json, true);
+                if ($json_data === null){
+                    die('Nem sikerült dekódolni a JSON file-t');
+                }
+            //Ellenőrzi, hogy az email benne van-e a listában
+            if (in_array($email,$json_data)){
+                echo "Ezt az email-t nem használhatod!";
+                }
+            else{
+                if ($this->reszletekModel->emailHozzadas($esemenyID, $email)) {
+                    // A hozzáadás sikerült, átirányítjuk a felhasználót a főoldalra
+                    header('location:' . URLROOT . '/main');
+                }
+                else {
+                    // A hozzáadás nem sikerült ezért vissza irányítjuk a főoldlra
+                    header('location:' . URLROOT . '/main');
+                }
             }
         }
+        
     }
 }
 
