9674e1f12ab7dd29e186c12d6ace2c7a3461febc
Almavagyok <nagybeni1134@gmail.com>
2024-09-27 13:54:43 +0200

    slideshow hozzáadása

--- app/views/main/index.php
+++ app/views/main/index.php
@@ -2,7 +2,43 @@
   require APPROOT . '/views/includes/head.php';
   require APPROOT . '/views/includes/navigation.php';
 ?>
+<main>
+<div class="slide">
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+
+  <img src="<?php echo URLROOT ?>/public/img/asd.jpg" style="width:100%">
+  <div class="text"><span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span></div>
+</div>
+
+<div class="mySlides fade">
+
+  <img src="<?php echo URLROOT ?>/public/img/asd.jpg" style="width:100%">
+   <div class="text"><span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span></div>
+</div>
+
+<div class="mySlides fade">
+
+  <img src="<?php echo URLROOT ?>/public/img/asd.jpg" style="width:100%">
+	<div class="text"><span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span></div>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">❮</a>
+<a class="next" onclick="plusSlides(1)">❯</a>
+<button id="myBtn" style="display: none"></button>
+</div>
+<br>
+</div>
 <div class="container">
+    
+    
     <?php if (count($data["main"]) > 0) : ?>
             <?php foreach ($data["main"] as $sor): ?>
                 <div class="box">
@@ -23,4 +59,5 @@
     <?php endif; ?>
 
 </div>
-
+<script src="<?php echo URLROOT ?>/public/js/script.js"></script>
+</main>
