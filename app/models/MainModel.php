897ca1fba1ee506411a58b33fd9ab568a3d45106
Huszka Adrian <adrian.huszka@gmail.com>
2024-09-28 11:39:07 +0200

    Refactor SQL query in MainModel.php to include grouping by event details

--- app/models/MainModel.php
+++ app/models/MainModel.php
@@ -10,11 +10,12 @@ class MainModel {
     // Főoldal adatainak lekérdezése
     public function kartyaLekerdezes() {
         $this->db->query('SELECT e.cim, e.leiras, e.kep, e.datum, e.id AS "esemeny_id", u.nev, t.neve, t.ferohely, count(j.email) as "jelentkezok"
-                          FROM esemenyek e
-                          INNER JOIN users u ON e.tanarID = u.id
-                          INNER JOIN tanterem t ON e.tanteremID = t.id
-                          LEFT JOIN jelentkezok j ON e.id = j.esemenyID
-                          WHERE e.torolt = 0');
+                        FROM esemenyek e
+                        INNER JOIN users u ON e.tanarID = u.id
+                        INNER JOIN tanterem t ON e.tanteremID = t.id
+                        LEFT JOIN jelentkezok j ON e.id = j.esemenyID
+                        WHERE e.torolt = 0
+                        GROUP BY e.cim, e.leiras, e.kep, e.datum, e.id, u.nev, t.neve, t.ferohely');
         $results = $this->db->resultSet();
         
         return $results;
