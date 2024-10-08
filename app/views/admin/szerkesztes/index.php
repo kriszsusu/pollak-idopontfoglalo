cb49d46af137dc87d5543540eebbe98000d0768d
Huszka Adrián Gábor <hadrian@pollak.hu>
2024-10-08 14:54:09 +0200

    Refactor select options in admin szerkesztes index.php

--- app/views/admin/szerkesztes/index.php
+++ app/views/admin/szerkesztes/index.php
@@ -16,53 +16,50 @@
 
                <h3 class="title">Esemény szerkesztése</h3>
 
-               <input type="hidden" name="id" value="<?php echo $data['esemeny']->$id ?>">
+               <input type="hidden" name="id" value="<?php echo $data['esemeny']->id ?>">
 
                <div class="text-input">
                     <i class="ri-user-fill"></i>
-                    <input type="text" name="cim" placeholder="Cím" maxlength="30" required value="<?php echo $data['esemeny']->cim ?>">
+                    <input type="text" name="cim" placeholder="Cím" maxlength="30" required value="<?php echo htmlspecialchars($data['esemeny']->cim); ?>">
                </div>
 
                <div class="text-input">
                     <i class="ri-lock-fill"></i>
-                    <input type="file" name="kep" placeholder="Kép" maxlength="500" value="<?php echo URLROOT; ?>/public/img/<?php echo $data['esemeny']->kep ?>" >
+                    <input type="file" name="kep" placeholder="Kép" maxlength="500">
                </div>
                <div class="text-input">
                     <i class="ri-lock-fill"></i>
-                    <input type="datetime-local" name="datum" placeholder="Időpont" required value="<?php echo $data['esemeny']->datum ?>">
+                    <input type="datetime-local" name="datum" placeholder="Időpont" required value="<?php echo $data['esemeny']->datum; ?>">
                </div>
 
                <div>
                     <select name="tanteremID" id="id-tanterem">
                         <?php foreach ($data['terem'] as $fajta): ?>
-                            <option class="marka" value="<?php echo $fajta->id; ?>" <?php $data['esemeny']->tanteremID == $fajta->id ? echo 'selected' : echo ''; ?> ><?php echo $fajta->neve; ?></option>
+                            <option class="marka" value="<?php echo $fajta->id; ?>" <?php echo ($data['esemeny']->tanteremID == $fajta->id) ? 'selected' : ''; ?>><?php echo htmlspecialchars($fajta->neve); ?></option>
                         <?php endforeach; ?>
                     </select>
                </div>
-
 
                <div>
                     <select name="tanar" id="id-tanar">
                         <?php foreach ($data['tanarok'] as $fajta): ?>
-                            <option class="marka" value="<?php echo $fajta->id; ?>" <?php $data['esemeny']->tanarID == $fajta->id ? echo 'selected' : echo ''; ?> ><?php echo $fajta->nev; ?></option>
+                            <option class="marka" value="<?php echo $fajta->id; ?>" <?php echo ($data['esemeny']->tanarID == $fajta->id) ? 'selected' : ''; ?>><?php echo htmlspecialchars($fajta->nev); ?></option>
                         <?php endforeach; ?>
                     </select>
                </div>
 
                <div>
-                    <select name="szak" id="id-tanterem">
+                    <select name="szak" id="id-szak">
                         <?php foreach ($data['szak'] as $fajta): ?>
-                            <option class="marka" value="<?php echo $fajta->id; ?>" <?php $data['esemeny']->szakID == $fajta->id ? echo 'selected' : echo ''; ?> ><?php echo $fajta->neve; ?></option>
+                            <option class="marka" value="<?php echo $fajta->id; ?>" <?php echo ($data['esemeny']->szakID == $fajta->id) ? 'selected' : ''; ?>><?php echo htmlspecialchars($fajta->neve); ?></option>
                         <?php endforeach; ?>
                     </select>
                </div>
 
                <div>
-                    <textarea name="leiras" id="" cols="40" rows="8" required><?php echo $data['esemeny']->leiras ?></textarea>
+                    <textarea name="leiras" cols="40" rows="8" required><?php echo htmlspecialchars($data['esemeny']->leiras); ?></textarea>
                </div>
 
-
-               
                <button type="submit" class="login-btn">Feltöltés</button>
           </div>
      </div>
