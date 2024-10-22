0a340ceb4468d59995e2f0cb213915d4e5dda58e
Huszka Adrián Gábor <hadrian@pollak.hu>
2024-10-22 10:41:48 +0200

    Refactor .gitignore and navigation.php files

--- app/views/includes/navigation.php
+++ app/views/includes/navigation.php
@@ -1,7 +1,10 @@
 <div class="navbar" id="navbarID">
-  <a href="<?php echo URLROOT?>" class="active">Főoldal</a>
-  <a href="<?php echo URLROOT?>/verseny" >Versenyek</a>
-  <a href="<?php echo URLROOT?>/kapcsolat">Kapcsolat</a>
+  <?php
+    $current_page = basename($_SERVER['REQUEST_URI']);
+  ?>
+  <a href="<?php echo URLROOT?>" class="<?php echo $current_page == '' || $current_page == 'pollak-idopontfoglalo' ? 'active' : ''; ?>">Főoldal</a>
+  <a href="<?php echo URLROOT?>/verseny" class="<?php echo $current_page == 'verseny' ? 'active' : ''; ?>">Versenyek</a>
+  <a href="<?php echo URLROOT?>/kapcsolat" class="<?php echo $current_page == 'kapcsolat' ? 'active' : ''; ?>">Kapcsolat</a>
   <a class="bejelentkezes" href="<?php echo URLROOT?>/admin">Bejelentkezés</a>
 
   <a href="javascript:void(0);" class="icon" onclick="navbar()">
