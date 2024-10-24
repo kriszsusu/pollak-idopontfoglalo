5e05173341f67fbfd19d6c066e309fc107348b8c
Huszka Adrián Gábor <hadrian@pollak.hu>
2024-10-24 14:01:57 +0200

    Refactor input placeholder in index.php

--- app/views/versenyreszletek/index.php
+++ app/views/versenyreszletek/index.php
@@ -43,7 +43,7 @@
                 <form  class="jelentkezes" id="teszt" method="post">
                     <input type="hidden" name="versenyID" value="<?php echo $data['Versenyreszletek']->esemeny_id ?>">
                     <input type="text" class="input" name="tanuloNeve" placeholder="Név">
-                    <input type="email" class="input" onkeyup="validate()" id="input" name="email" placeholder="E-mail cím">
+                    <input type="email" class="input" onkeyup="validate()" id="input" name="email" placeholder="Felkészítő tanár e-mail címe">
                     <input type="text" class="input" name="tanarNeve" placeholder="Felkészítő tanár neve">
                     <select name="iskola" id="id-iskolak" class="input" style="height: 35px; width: 360px;" onchange="iskolak(this)">
                         <option value="-1">Válassz Iskolát!</option>
